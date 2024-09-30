bd396494b701a538fba4ca9297e849f65a5080ca
ValerioViteritti <vviteritti@gmail.com>
2024-09-30 15:49:53 +0200

    feat: base project

---
+++ resources/views/admin/partials/aside.blade.php
@@ -0,0 +1,13 @@
+<aside class="text-bg-dark">
+
+    <ul>
+        <li>
+            <a href="{{ route('admin.home') }}"><i class="fa-solid fa-house"></i> Home</a>
+            <a href="{{ route('admin.posts.index') }}"><i class="fa-solid fa-list"></i> elenco post</a>
+            <a href="{{ route('admin.posts.create') }}"><i class="fa-solid fa-newspaper"></i>Nuovo Post</a>
+            <a href="{{ route('admin.posts.create') }}"><i class="fa-solid fa-pen-to-square"></i>Gestione Categorie</a>
+            <a href="{{ route('admin.categoryPosts') }}"><i class="fa-solid fa-layer-group"></i>Post per categorie</a>
+        </li>
+    </ul>
+
+</aside>
